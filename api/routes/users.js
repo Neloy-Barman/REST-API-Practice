b52a25966a8764520bcf5de85ff15c74ff7adffd
Neloy-Barman <neloycareer018@gmail.com>
2023-08-14 16:30:29 +0600

    login added

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require("../models/user");
+const { token } = require('morgan');
+
+router.get("/", (req, res, next) => {
+    User.find().exec().then(result => {
+        res.status(200).json({
+            users: result
+        });
+    }).catch(error => {
+        error: error
+    });
+});
 
 router.post("/signup", (req, res, next) => {
     User.find({ email: req.body.email }).exec().then(user => {
@@ -36,6 +48,44 @@ router.post("/signup", (req, res, next) => {
             });
         }
     }).catch();
+});
+
+router.post("/login", (req, res, next) => {
+    User.find({ email: req.body.email }).exec().then(user => {
+        if (user.length < 1) {
+            res.status(404).json({
+                message: "Auth failed!"
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if (err) {
+                return res.status(401).json({
+                    message: "Auth failed!"
+                });
+            }
+            if (result) {
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, "secret", {
+                    expiresIn: "1h",
+
+                });
+                return res.status(200).json({
+                    message: "Auth successful",
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message: "Auth failed",
+                token: token,
+            });
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
+    });
 });
 
 
